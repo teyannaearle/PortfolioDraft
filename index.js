81af419d44475d761bccb5a891a090e8a371945d
Teyanna Earle <teyonna.earle92@gmail.com>
2021-03-05 02:47:35 -0500

    Change to Single Page/Add Content

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    proj1Info()
+ 
+
+})
+
+// function proj1Info(){
+ 
+   
+//     document.addEventListener("click", () => {
+//         const button = document.getElementById("proj1")
+//         let para = document.createElement("p")
+//         para.textContent = `Description`
+//         button.appendChild(para)
+//         let gitLink = document.createElement("a")
+//         gitLink.textContent = `click me`
+
+
+
+//     })
+
+// }
+
+
